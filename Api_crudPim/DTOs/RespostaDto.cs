4eac563b592bad3ea867e0ebaea43531e8bf0093
henriquexcj <110138150+henriquexcj@users.noreply.github.com>
2025-11-11 23:42:22 -0300

    retirando a api

--- Api_crudPim/DTOs/RespostaDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Api_crudPim.DTOs
-{
-    public class RespostaDto
-    {
-        public int ChamadoID { get; set; }
-        public int UsuarioID { get; set; }
-        public string Mensagem { get; set; }
-    }
-}
